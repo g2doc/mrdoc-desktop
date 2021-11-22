b41aade0b47979ef25e450d4977be4096d566be8
zmister <zmister@qq.com>
2021-11-22 20:01:20 +0800

    添加新建文集方法

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,54 +1,75 @@
 <template>
-  <!-- <el-container style="background-color: #f5f5f5;height:100%;"> -->
-    <el-row style="height:100%">
-      <!-- 文集列表 -->
-      <el-col :span="5" class="project-list-container">
-          <div class="project-list-name">文集列表 <i class="refresh-project-list" :class="refresh_project_icon" @click="refreshProjectList()"></i></div>
-          <div class="create-btn-div">
-            <el-button type="primary" size="small" round>新建文集</el-button>
-            <el-button type="primary" size="small" round>新建文档</el-button>
-          </div>
-          <el-scrollbar class="item-scrollbar" v-loading="project_list_loading">
-            <div class="project-item" :class="current_project==item.name?'current-project':''" v-for="(item) in project_list" :key="item.id" @click="getProjectDocs(item.id,item.name)">
-              <i class="el-icon-folder-opened"></i> {{item.name}}
-            </div>
-          </el-scrollbar>
-          <div class="project-list-bottom">合计：{{projectCnt}} 个文集</div>
-      </el-col>
-      <!-- 文档列表 -->
-      <el-col :span="6" class="doc-list-container" v-if="noDocList">
-        <el-empty description="本来无一物" ></el-empty>
-      </el-col>
-      <el-col :span="6" class="doc-list-container" v-loading="doc_list_loading" v-else>
-          <div class="doc-search-input">
-            <el-input
-              placeholder="搜索文档"
-              prefix-icon="el-icon-search"
-              >
-            </el-input>
-          </div>
-          <el-scrollbar class="item-scrollbar">
-            <div v-for="(item) in current_doc_list" :key="item.id" class="doc-item" @click="getDoc(item.id)">
-              <i class="el-icon-tickets"></i>
-               {{item.name}}
-              <div class="doc-item-time">{{item.create_time | dateFomart}}</div>
-            </div>
-          </el-scrollbar>
-      </el-col>
-      <!-- 编辑器 -->
-      <el-col :span="13" class="editor-container" v-loading="doc_loading">
-        <div class="doc-info">
-          <el-breadcrumb separator="/">
-            <el-breadcrumb-item>{{current_project}}</el-breadcrumb-item>
-            <el-breadcrumb-item>{{current_doc.name}}</el-breadcrumb-item>
-          </el-breadcrumb>
+  <el-row style="height:100%">
+    <!-- 文集列表 -->
+    <el-col :span="5" class="project-list-container">
+        <div class="project-list-name">文集列表 <i class="refresh-project-list" :class="refresh_project_icon" @click="refreshProjectList()"></i></div>
+        <div class="create-btn-div">
+          <el-button type="primary" size="small" round @click="dialogCreateProjectVisible = true">新建文集</el-button>
+          <el-button type="primary" size="small" round>新建文档</el-button>
         </div>
-        <div class="editor">
-          <textarea id="editor"></textarea>
+        <el-scrollbar class="item-scrollbar" v-loading="project_list_loading">
+          <div class="project-item" :class="current_project==item.name?'current-project':''" v-for="(item) in project_list" :key="item.id" @click="getProjectDocs(item.id,item.name)">
+            <i class="el-icon-folder-opened"></i> {{item.name}}
+          </div>
+        </el-scrollbar>
+        <div class="project-list-bottom">合计：{{projectCnt}} 个文集</div>
+    </el-col>
+    <!-- 文集列表结束 -->
+    <!-- 文档列表 -->
+    <el-col :span="6" class="doc-list-container" v-if="noDocList">
+      <el-empty description="本来无一物" ></el-empty>
+    </el-col>
+    <el-col :span="6" class="doc-list-container" v-loading="doc_list_loading" v-else>
+        <div class="doc-search-input">
+          <el-input
+            placeholder="搜索文档"
+            prefix-icon="el-icon-search"
+            >
+          </el-input>
         </div>
-      </el-col>
-    </el-row>
-  <!-- </el-container> -->
+        <el-scrollbar class="item-scrollbar">
+          <div v-for="(item) in current_doc_list" :key="item.id" class="doc-item" @click="getDoc(item.id)">
+            <i class="el-icon-tickets"></i>
+              {{item.name}}
+            <div class="doc-item-time">{{item.create_time | dateFomart}}</div>
+          </div>
+        </el-scrollbar>
+    </el-col>
+    <!-- 文档列表结束 -->
+    <!-- 编辑器 -->
+    <el-col :span="13" class="editor-container" v-loading="doc_loading">
+      <div class="doc-info">
+        <el-breadcrumb separator="/">
+          <el-breadcrumb-item>{{current_project}}</el-breadcrumb-item>
+          <el-breadcrumb-item>{{current_doc.name}}</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <div class="editor">
+        <textarea id="editor"></textarea>
+      </div>
+    </el-col>
+    <!-- 编辑器结束 -->
+    <!-- 新建文集弹出框 -->
+    <el-dialog title="新建文集" :visible.sync="dialogCreateProjectVisible">
+      <el-form :model="createProjectForm">
+        <el-form-item label="文集名称">
+          <el-input v-model="createProjectForm.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="文集简介">
+          <el-input type="textarea" v-model="createProjectForm.desc"></el-input>
+        </el-form-item>
+        <el-form-item label="文集权限">
+          <el-radio v-model="createProjectForm.role" label="0">私密</el-radio>
+          <el-radio v-model="createProjectForm.role" label="1">公开</el-radio>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogCreateProjectVisible = false" size="small" round>取 消</el-button>
+        <el-button type="primary" @click="createProject" size="small" round>确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 新建文集弹出框结束 -->
+  </el-row>
 </template>
 
 <script>
@@ -81,6 +102,8 @@ export default {
       noDoc:true, // 文档状态
       host_url:'',
       user_token : '',
+      dialogCreateProjectVisible:false,// 新建文集弹出框可访问性
+      createProjectForm:{name:'',desc:'',role:'1'}
     }
   },
   watch: {
@@ -178,6 +201,38 @@ export default {
       this.project_list_loading = true;
       this.getProjectList();
       this.refresh_project_icon = 'el-icon-refresh'
+    },
+    // 新建文集
+    createProject(){
+      if(this.createProjectForm.name == ''){
+        this.$message({message:"文集名称不能为空！",type:"error"})
+      }else{
+        fetch(this.host_url + '/api/create_project/?token='+this.user_token,{
+          method:'POST',
+          mode:'cors',
+          body:JSON.stringify({
+            name:this.createProjectForm.name,
+            desc:this.createProjectForm.desc,
+            role:this.createProjectForm.role
+          })
+        })
+        .then((r)=>{
+          return r.json()
+        })
+        .then(r=>{
+          console.log(r)
+          if(r.status){
+            this.dialogCreateProjectVisible = false;
+            this.$message({message:"创建成功",type:"success"})
+            this.refreshProjectList()
+          }else{
+            this.$message({message:r.data,type:"error"})
+          }
+        })
+        .catch(error=>{
+          console.log(error)
+        })
+      }
     }
   },
   filters:{
@@ -239,6 +294,7 @@ export default {
     text-align: center;
   }
   .project-list-bottom{
+    margin-top: 5px;
     margin-left: 5px;
     font-size: 12px;
     /* text-align: center; */
